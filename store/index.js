4d40b981008a53bb58c3ae15394a68bf7f76437c
Whapow <joshuavancamp@gmail.com>
2018-06-13 15:54:40 -0400

    add nuxtServerInit

---
+++ store/index.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import { getField, updateField } from 'vuex-map-fields'
+
+export const state = function(){
+  return ({
+  })
+}
+
+export const getters = {
+  getField,
+}
+
+export const mutations = {
+  updateField,
+}
+
+export const actions = {
+  async nuxtServerInit({dispatch}){
+    await dispatch('campaigns/init')
+  },
+}

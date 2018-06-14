b9e49ff23be88ef5e42af2fca305a24bfd4cfdeb
Whapow <joshuavancamp@gmail.com>
2018-06-13 23:59:04 -0400

    add sessions store

---
+++ store/session.js
@@ -0,0 +1,32 @@
+import Vue from 'vue'
+import { getField, updateField } from 'vuex-map-fields'
+
+export const state = function(){
+  return ({
+    currentUser: null,
+  })
+}
+
+export const getters = {
+  getField,
+  session: state => { return state.currentUser }
+}
+
+export const mutations = {
+  updateField,
+}
+
+export const actions = {
+  async login({commit}, loginParams){
+    await this.$axios.post('/api/login', {session: loginParams}).then(response => {
+      let user = response.data.data.attributes
+      commit('updateField', {path: 'currentUser', value: user})
+    })
+  },
+
+  async logout({commit}){
+    await this.$axios.delete('/api/logout').then(response => {
+      commit('updateField', {path: 'currentUser', value: null})
+    })
+  }
+}

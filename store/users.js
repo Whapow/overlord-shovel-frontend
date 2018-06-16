811c745315146ddc5ade8a20c204a5e467db0edf
Whapow <joshuavancamp@gmail.com>
2018-06-16 15:59:21 -0400

    add users store

---
+++ store/users.js
@@ -0,0 +1,24 @@
+import { getField, updateField } from 'vuex-map-fields'
+
+export const state = function(){
+  return ({
+    collection: {}
+  })
+}
+
+export const getters = {
+  getField,
+}
+
+export const mutations = {
+  updateField,
+}
+
+export const actions = {
+  async init({commit}){
+    await this.$axios.get('/users').then(response => {
+      let users = Object.assign({}, ...response.data.data.map(u => {return {[u.id]: u.attributes} }) )
+      commit('updateField', {path: 'collection', value: users })
+    })
+  },
+}

a41063bb88c488a41104a31e4a091629ab58c6df
Whapow <joshuavancamp@gmail.com>
2018-06-15 23:27:53 -0400

    add check for campaignId on characters store

--- store/characters.js
+++ store/characters.js
@@ -4,6 +4,7 @@ import { getField, updateField } from 'vuex-map-fields'
 export const state = function(){
   return ({
     collection: {},
+    campaignId: null
   })
 }
 
@@ -27,6 +28,10 @@ export const mutations = {
 
 export const actions = {
   async init({commit}, params){
+    if (params.campaign_id){
+      let campaignId = Number(params.campaign_id)
+      commit('updateField', {path: 'campaignId', value: campaignId }) 
+    }
     await this.$axios.get('/characters').then(response => {
       let characters = Object.assign({}, ...response.data.data.map(c => {return {[c.id]: c.attributes} }) )
       commit('updateField', {path: 'collection', value: characters })

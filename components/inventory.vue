f50d72fa044bf775c7d5eeae7f2a9d40219c4677
Joshua Van Camp <joshuavancamp@gmail.com>
2020-07-08 15:56:02 -0400

    replace draggable-vue with custom drag-drop

--- components/inventory.vue
+++ components/inventory.vue
@@ -4,20 +4,31 @@
       .panel-header
         span.inventory-title {{ inventory.name }}
         button(@click="newStack") +
-      //- p {{ inventory | totalValue }}g
-      draggable.grid.inventory(@end="moveItem", :value="inventory.stacks", :options="{group: 'stacks'}", :id="inventory.id" )
-        stack.item(v-for="stack in inventory.stacks", v-if="stacks[stack.id]", 
-          :key="stack.id", :id="stack.id", :stackReference="stack")
+      div.grid.inventory
+        div.slot(
+          v-for="slotIndex in slotsRequiredForFullRow"
+          :key="slotIndex"
+          draggable
+          @dragstart="startDrag($event,arrangedStacks[slotIndex])"
+          @drop='onDrop($event, slotIndex, inventory, arrangedStacks[slotIndex])'
+          @dragover.prevent
+          @dragenter.prevent
+        )
+          stack.item.drag-el(
+            v-if="arrangedStacks[slotIndex]", 
+            :id="arrangedStacks[slotIndex].id", 
+            :stackReference="arrangedStacks[slotIndex]"
+          )
+          div.item(v-else)
 </template>
 
 <script>
   import { mapGetters, mapActions } from 'vuex'
-  import draggable from 'vuedraggable'
   import stack from '~/components/stack'
 
   export default {
     props: ['inventoryReference', 'owner'],
-    components: {draggable, stack},
+    components: {stack},
     computed: {
       ...mapGetters({
         currentUser: 'session/currentUser',
@@ -27,6 +38,19 @@
       }),
       inventory(){
         return this.inventories[this.inventoryReference.id]
+      },
+      highestPosition(){
+        let highest = 1
+        this.inventory.stacks.forEach(stack => { stack.position > highest && (highest = stack.position) })
+        return highest
+      },
+      slotsRequiredForFullRow(){
+        return Math.ceil((this.highestPosition + 1) / 5) * 5
+      },
+      arrangedStacks(){
+        let arranged = {}
+        this.inventory.stacks.forEach(stack => {return arranged[stack.position] = stack})
+        return arranged
       }
     },
     methods:{
@@ -43,6 +67,33 @@
       },
       newStack(){
         this.addStack(this.inventory)
+      },
+      startDrag (evt, stack) {
+        evt.dataTransfer.dropEffect = 'move'
+        evt.dataTransfer.effectAllowed = 'move'
+        evt.dataTransfer.setData('stack', JSON.stringify(stack))
+      },
+      onDrop (evt, position, inventory, existingStack = {}) {
+        let movedStack = JSON.parse(evt.dataTransfer.getData('stack'))
+        if (_.isEmpty(existingStack)) {
+          // Move
+          console.log("Move: ", movedStack)
+          console.log("To: ", inventory)
+          console.log("Position: ", position)
+        } else if (existingStack.item_id == movedStack.item_id) {
+          // Combine
+          console.log("Combine: ", movedStack)
+          console.log("With: ", existingStack)
+        } else {
+          if (inventory.id == movedStack.inventory_id ){
+            // Swap
+            console.log("Swap: ", movedStack)
+            console.log("With: ", existingStack)
+          } else {
+            // No-op
+            console.log("Not Yet Implemented")
+          }
+        }
       }
     },
     filters: {
@@ -64,20 +115,23 @@
     border-style: solid;
   }
   .inventory {
+    overflow: auto;
+    height: 25rem;
     padding: 0;
     margin: 0;
     grid-template-rows: repeat(5, 1fr);
     grid-template-columns: repeat(5, 1fr);
+    background-color: lightgray;
   }
   .item {
     min-height: 5rem;
     min-width: 5rem;
     max-height: 5rem;
     max-width: 5rem;
+    background-color: white;
   }
   .inventory-title {
     font-size: 1.5rem;
   }
-
 </style>
 
